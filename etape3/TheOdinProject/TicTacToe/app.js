144786168ef69941849fa817b7c98877d1e7068f
lauric-h <h.lauric@outlook.com>
2020-12-20 13:26:15 +0100

    nouveaux ajouts

--- etape3/TheOdinProject/TicTacToe/app.js
+++ etape3/TheOdinProject/TicTacToe/app.js
@@ -1,27 +1,43 @@
+const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 const Gameboard = (() =>{
   const boardContainer = document.querySelector('.boardContainer')
-  const board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  
 
   const renderBoard = () => {
-    board.forEach(box => {
-      let cell = document.createElement('div');
-      cell.innerHTML = box;
-      boardContainer.appendChild(cell);
-    })
+    boardContainer.innerHTML = board.map((box, i) => {
+      return `<div data-id='${i}' class='boardCell'>${box}</div>`;
+    }).join('');
   }
 
   return {
     renderBoard
   }
 
-
-
-
-
 })()
 
+function placeMarker() {
+  const boardContainer = document.querySelector('.boardContainer')
+  boardContainer.addEventListener('click', function(e) {
+    console.log(e.target);
+    if (e.target.textContent === '0') {
+      e.target.textContent = 'X';
+      console.log(e.target.dataset.id); 
+      board[e.target.dataset.id] = 'X';
+    } else if (e.target.textContent === 'X') {
+      console.log('not possible')
+    }
+  })
+}
+
+function checkNearbyCells(index) {
+  let 
+}
+
+
+
 // générer le gameboard
-// Faire en sorte que quand on clique sur une cell, ça : 
+// // Faire en sorte que quand on clique sur une cell, ça : 
   // change la cell en 1
   // change le contenu du board en un 
 // Faire en sorte qu'on ne puisse pas cliquer sur une cell qui est déjà cliquée
